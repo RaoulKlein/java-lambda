ce1bab756147aa0006c5a55ca2998516578caba2
RaoulKlein <raoul.klein84@gmail.com>
2019-05-06 21:53:15 +0200

    Admin - Initial Commit

---
+++ src/main/java/org/klein/raoul/App.java
@@ -0,0 +1,13 @@
+package org.klein.raoul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
